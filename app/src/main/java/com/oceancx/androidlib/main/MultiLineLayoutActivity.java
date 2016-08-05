bb785f4ee3e5beb3f2996950bde1edaad5b59b94
oceancx <oceancx@gmail.com>
2016-08-05 19:23:15 +0800

    updae Multilinearlayout

--- app/src/main/java/com/oceancx/androidlib/main/MultiLineLayoutActivity.java
+++ app/src/main/java/com/oceancx/androidlib/main/MultiLineLayoutActivity.java
@@ -1,9 +1,13 @@
 package com.oceancx.androidlib.main;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,6 +20,7 @@ import com.oceancx.androidlib.ui.widget.MultiLineLayout;
 public class MultiLineLayoutActivity extends AppCompatActivity {
 
     MultiLineLayout multiLineLayout;
+    MultiLineLayout multiLineLayout2;
 
     String[] tags = {"国企", "金融", "IT", "大学生", "其他学生", "媒体", "餐饮", "零售", "自由职业", "教育", "医药", "汽车", "制造业", "摄影", "运动", "丽人", "美食", "旅游", "电影", "k歌", "温泉", "美容 spa", "摄影", "运动健身", "商品", "单身", "热恋", "结婚", "有宝宝", "国企", "金融", "IT", "大学生", "其他学生", "媒体", "餐饮", "零售", "自由职业", "教育", "医药", "汽车", "制造业", "摄影", "运动", "丽人", "美食", "旅游", "电影", "k歌", "温泉", "美容 spa", "摄影", "运动健身", "商品", "单身", "热恋", "结婚", "有宝宝", "国企", "金融", "IT", "大学生", "其他学生", "媒体", "餐饮", "零售", "自由职业", "教育", "医药", "汽车", "制造业", "摄影", "运动", "丽人", "美食", "旅游", "电影", "k歌", "温泉", "美容 spa", "摄影", "运动健身", "商品", "单身", "热恋", "结婚", "有宝宝"};
 
@@ -23,12 +28,15 @@ public class MultiLineLayoutActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.multilinelayout_activity);
-        multiLineLayout = (MultiLineLayout) findViewById(R.id.multi_line1);
-        multiLineLayout.setWeightSum((float) 6.1);
 
+        multiLineLayout = (MultiLineLayout) findViewById(R.id.multi_line1);
+        multiLineLayout2 = (MultiLineLayout) findViewById(R.id.multi_line2);
+
+        multiLineLayout.setWeightSum((float) 6.1);
+        multiLineLayout2.setWeightSum(5.9f);
         for (int i = 0; i < tags.length; i++) {
             TextView tag = new TextView(this);
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
             lp.setMargins(10, 30, 10, 0);
             tag.setLayoutParams(lp);
             tag.setText(tags[i]);
@@ -38,8 +46,24 @@ public class MultiLineLayoutActivity extends AppCompatActivity {
             tag.setTextColor((int) (Math.random() * 0xffffff) | 0xff000000);
 
             multiLineLayout.addView(tag);
-        }
-        multiLineLayout.requestLayout();
 
+            tag = new TextView(this);
+            lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1);
+            lp.setMargins(10, 30, 10, 0);
+            tag.setLayoutParams(lp);
+            tag.setText(tags[i]);
+            tag.setTextSize(14);
+            tag.setGravity(Gravity.CENTER);
+            tag.setBackgroundDrawable(getResources().getDrawable(R.drawable.tag_tv_bg2));
+            tag.setTextColor((int) (Math.random() * 0xffffff) | 0xff000000);
+
+            multiLineLayout2.addView(tag);
+        }
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
     }
 }
--- app/src/main/java/com/oceancx/androidlib/ui/widget/MultiLineLayout.java
+++ app/src/main/java/com/oceancx/androidlib/ui/widget/MultiLineLayout.java
@@ -1,7 +1,9 @@
 package com.oceancx.androidlib.ui.widget;
 
 import android.content.Context;
+import android.os.Debug;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -23,7 +25,7 @@ public class MultiLineLayout extends LinearLayout {
      * 然后 还按照之前的布局方法进行布局即可
      */
     ArrayList<Integer> mMaxHeights;
-
+    ArrayList<Integer> mMaxWidths;
     public MultiLineLayout(Context context) {
         this(context, null);
     }
@@ -35,6 +37,7 @@ public class MultiLineLayout extends LinearLayout {
     public MultiLineLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mMaxHeights = new ArrayList<>();
+        mMaxWidths=new ArrayList<>();
     }
 
     /**
@@ -48,7 +51,96 @@ public class MultiLineLayout extends LinearLayout {
      */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (getOrientation()== VERTICAL) {
+            measureVertical(widthMeasureSpec, heightMeasureSpec);
+        } else {
+            measureHorizontal(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
 
+    private void measureVertical(int widthMeasureSpec, int heightMeasureSpec) {
+        int parentSize = MeasureSpec.getSize(heightMeasureSpec);
+        int parentMode = MeasureSpec.getMode(heightMeasureSpec);
+        /**
+         * 求出这个变量 就能确定视图的宽度了
+         */
+        int widthSize = 0;
+        int mMaxWidthSeparatorIndex = 0;
+        mMaxWidths.clear();
+        float mWeightSum = getWeightSum();
+
+        boolean determinedSpecHeight = false;
+        int determinedHeight = getPaddingTop() + getPaddingBottom();
+        int maxChildWidth = 0;
+        int weightConsumed=0;
+        int lineHeight = getPaddingTop() + getPaddingBottom();
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            if (child.getVisibility() != GONE) {
+                /**
+                 * 根据weight 更改measureSpec
+                 * 有weight的情况下 就按weight测试的布局
+                 */
+                LayoutParams lp = (LayoutParams) child.getLayoutParams();
+                int childHeightMeasureSpec = heightMeasureSpec;
+                double ratio;
+                if (lp.weight != 0 && mWeightSum > 0) {
+                    ratio = lp.weight * 1.0f / mWeightSum;
+                    double size = parentSize * ratio;
+                    if (i % 2 == 0)
+                        size = Math.floor(size);
+                    else
+                        size = Math.ceil(size);
+                    childHeightMeasureSpec = MeasureSpec.makeMeasureSpec((int) size, parentMode);
+                    weightConsumed  += lp.weight;
+                }
+
+                measureChildWithMargins(child, widthMeasureSpec, 0, childHeightMeasureSpec, 0);
+                /**
+                 * 测量算法：
+                 * 1. 确定行高
+                 * 2. 一个一个确定孩子高度
+                 * 3. 按高度布局，超过行宽就换行
+                 */
+                int childSpace = child.getMeasuredHeight() + lp.topMargin+ lp.bottomMargin;
+
+                if (!determinedSpecHeight) {
+                    determinedHeight += childSpace;
+                    if (determinedHeight >= parentSize ||weightConsumed>=mWeightSum) {
+                        determinedHeight = parentSize;
+                        determinedSpecHeight = true;
+                    }
+                }
+                lineHeight += childSpace;
+                if (determinedSpecHeight && lineHeight > determinedHeight) {
+                    lineHeight = getPaddingTop() + getPaddingBottom();
+                    widthSize += maxChildWidth;
+                    int mMaxWidthsSize = mMaxWidths.size();
+                    for (int j = mMaxWidthsSize; j < mMaxWidthSeparatorIndex; j++)
+                        mMaxWidths.add(maxChildWidth);
+                    maxChildWidth = 0;
+                    i--;
+                } else {
+                    maxChildWidth = Math.max(maxChildWidth, lp.leftMargin + lp.rightMargin+ child.getMeasuredWidth());
+                    mMaxWidthSeparatorIndex++;
+                }
+            }
+        }
+        widthSize += maxChildWidth;
+        int mMaxWidthsSize = mMaxWidths.size();
+        for (int j = mMaxWidthsSize; j < mMaxWidthSeparatorIndex; j++)
+            mMaxWidths.add(maxChildWidth);
+
+        widthSize += getPaddingLeft() + getPaddingRight();
+
+        if (determinedSpecHeight) {
+            setMeasuredDimension(resolveSize(widthSize,widthMeasureSpec), resolveSize(determinedHeight,   heightMeasureSpec));
+        } else {
+            setMeasuredDimension(resolveSize(widthSize, widthMeasureSpec), resolveSize(lineHeight, heightMeasureSpec));
+        }
+    }
+
+    void measureHorizontal(int widthMeasureSpec, int heightMeasureSpec) {
         int parentSize = MeasureSpec.getSize(widthMeasureSpec);
         int parentMode = MeasureSpec.getMode(widthMeasureSpec);
         /**
@@ -62,6 +154,7 @@ public class MultiLineLayout extends LinearLayout {
         boolean determinedSpecWidth = false;
         int determinedWidth = getPaddingLeft() + getPaddingRight();
         int maxChildHeight = 0;
+        int weightConsumed=0;
         int lineWidth = getPaddingLeft() + getPaddingRight();
         for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
@@ -81,6 +174,7 @@ public class MultiLineLayout extends LinearLayout {
                     else
                         size = Math.ceil(size);
                     childWidthMeasureSpec = MeasureSpec.makeMeasureSpec((int) size, parentMode);
+                    weightConsumed+=lp.weight;
                 }
 
                 measureChildWithMargins(child, childWidthMeasureSpec, 0, heightMeasureSpec, 0);
@@ -94,13 +188,13 @@ public class MultiLineLayout extends LinearLayout {
 
                 if (!determinedSpecWidth) {
                     determinedWidth += childSpace;
-                    if (determinedWidth >= parentSize) {
+                    if (determinedWidth >= parentSize || weightConsumed>=mWeightSum) {
                         determinedWidth = parentSize;
                         determinedSpecWidth = true;
                     }
                 }
                 lineWidth += childSpace;
-                if (determinedSpecWidth && lineWidth > parentSize) {
+                if (determinedSpecWidth && lineWidth > determinedWidth) {
                     lineWidth = getPaddingLeft() + getPaddingRight();
                     heightSize += maxChildHeight;
                     int mMaxHeightsSize = mMaxHeights.size();
@@ -129,7 +223,40 @@ public class MultiLineLayout extends LinearLayout {
     }
 
     @Override
-    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (getOrientation()== VERTICAL) {
+            layoutVertical(l, t, r, b);
+        } else {
+            layoutHorizontal(l, t, r, b);
+        }
+    }
+
+    void layoutVertical(int left, int top, int right, int bottom) {
+        int startLeft, startTop;
+        startLeft = getPaddingLeft();
+        startTop = getPaddingTop();
+        int maxWidth = 0;
+        for (int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            if (child.getVisibility() != GONE) {
+                LayoutParams lp = (LayoutParams) child.getLayoutParams();
+                int childSpace = child.getMeasuredHeight() + lp.topMargin+ lp.bottomMargin;
+                if (startTop+ childSpace + getPaddingBottom() > getMeasuredHeight()) {
+                    startLeft += maxWidth;
+                    startTop = getPaddingTop();
+                    maxWidth = getPaddingLeft();
+                }
+                int layoutLeft, layoutTop;
+                layoutLeft = startLeft + mMaxWidths.get(i)-child.getMeasuredWidth()-lp.rightMargin;
+                layoutTop = startTop + lp.topMargin;
+                child.layout(layoutLeft, layoutTop, layoutLeft + child.getMeasuredWidth(), layoutTop + child.getMeasuredHeight());
+                maxWidth = Math.max(maxWidth, child.getMeasuredWidth() + lp.leftMargin+ lp.rightMargin);
+                startTop += childSpace;
+            }
+        }
+    }
+
+    void layoutHorizontal(int left, int top, int right, int bottom) {
         int startLeft, startTop;
         startLeft = getPaddingLeft();
         startTop = getPaddingTop();
@@ -153,6 +280,4 @@ public class MultiLineLayout extends LinearLayout {
             }
         }
     }
-
-
 }
